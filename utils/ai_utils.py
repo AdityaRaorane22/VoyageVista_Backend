c1221c1264c52ad40b08efe2ecbf4e72e742d68e
Aditya Raorane <145953001+AdityaRaorane22@users.noreply.github.com>
2025-10-22 14:41:32 +0530

    Update ai_utils.py

--- utils/ai_utils.py
+++ utils/ai_utils.py
@@ -1,13 +1,14 @@
 import os
-from google import genai
+import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key=GEMINI_API_KEY)
+
+genai.api_key = GEMINI_API_KEY
 
 def generate_text(prompt: str):
-    response = client.models.generate_content(
+    response = genai.Client().models.generate_content(
         model="gemini-2.0-flash-exp",
         contents=prompt
     )
