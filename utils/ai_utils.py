09b3c805849a2b4b633b8809c678b636b17a19c0
Aditya Raorane <145953001+AdityaRaorane22@users.noreply.github.com>
2025-10-22 15:02:17 +0530

    Update ai_utils.py

--- utils/ai_utils.py
+++ utils/ai_utils.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-from google import genai
+import google.generativeai as genai
 
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-genai_client = genai.Client(api_key=GEMINI_API_KEY)
+genai.configure(api_key=GEMINI_API_KEY)
